97b62b0a80a7e2f29d4b8fb9f78feda22066da1c
giorgiacharlier <99804362+giorgiacharlier@users.noreply.github.com>
2025-09-16 21:50:23 +0200

    Added API to the project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function showCity(response) {
+  showTemperature = document.querySelector("#temperature");
+  actualTemperature = response.data.temperature.current;
+  let displayValue = document.querySelector("#city");
+
+  displayValue.innerHTML = response.data.city;
+  showTemperature.innerHTML = Math.round(actualTemperature);
+}
+
+function searchCity(city) {
+  let apiKey = "c703302f0df7o6btdae0e7937b001fe4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(showCity);
+}
+
 function handleSearchValue(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let displayValue = document.querySelector("#city");
-  displayValue.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
